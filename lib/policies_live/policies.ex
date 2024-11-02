614d7b954d12fc415fe760bcb87f6ebb8442d5cd
Dan Bowles <daniel.bowles@gmail.com>
2024-11-01 20:05:41 -0400

    :memo: Start of paging work

--- lib/policies_live/policies.ex
+++ lib/policies_live/policies.ex
@@ -14,12 +14,28 @@ defmodule PoliciesLive.Policies do
     |> Repo.all()
   end
 
+  def list_policies_with_count(opts) do
+    q = from(p in Policy) |> filter(opts)
+    count = Repo.aggregate(q, :count)
+
+    policies = q |> sort(opts) |> paginate(opts) |> Repo.all()
+
+    %{policies: policies, count: count}
+  end
+
   defp filter(query, opts) do
     query
     |> filter_by_policy_number(opts)
     |> filter_by_first_name(opts)
     |> filter_by_last_name(opts)
   end
+
+  defp paginate(query, %{page: page, page_size: page_size})
+       when page > 0 and page_size > 0 do
+    query |> limit(^page_size) |> offset(^page_size * (^page - 1))
+  end
+
+  defp paginate(query, _opts), do: query
 
   defp filter_by_policy_number(query, %{policy_number: policy_number})
        when policy_number !== nil do
